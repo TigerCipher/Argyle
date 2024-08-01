896959cbf9470bc59eacb4735de3ad026ad0ffff
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-07-31 23:17:33 -0500

    initial project setup

---
+++ ArgyleCore/src/Argyle.cpp
@@ -0,0 +1,67 @@
+﻿// ------------------------------------------------------------------------------
+//
+// Argyle
+//    Copyright 2024 Matthew Rogers
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// File Name: Argyle
+// Date File Created: 07/31/2024
+// Author: Matt
+//
+// ------------------------------------------------------------------------------
+#include "Argyle.h"
+
+#include <iostream>
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
+void argyle::core::test_glfw()
+{
+    std::cout << "Initializing GLFW...\n";
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return;
+    }
+    std::cout << "GLFW initialized\n";
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World!", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << '\n';
+        // print error
+        std::cout << glewGetErrorString(glewInit()) << '\n';
+        return;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Argyle Engine
+---
+Argyle Engine is a game engine written in C++ and OpenGL. It is designed to be a simple and easy to use engine for creating 2D games. The engine is still in development and is not yet ready for use.
+
+Currently, the focus is on Windows, however Linux may be supported in the future. There are no plans at the moment to support other rendering APIs such as DirectX or MacOS.
+
+---
+
+This project uses VCPKG. Building in visual studio with the supplied project files will install the needed dependencies. But first you must ensure that you have VCPKG installed and that the VCPKG_ROOT environment variable is set to the root directory of VCPKG.
+
+To add a new dependency, use the following command within the project it will be used for.
+```
+vcpkg add port <dependency name>
+```
+Example:
+```
+Microsoft Windows [Version 10.0.22631.3958]
+(c) Microsoft Corporation. All rights reserved.
+
+D:\Argyle\ArgyleCore>vcpkg add port glfw
+Succeeded in adding ports to vcpkg.json file.
+```
